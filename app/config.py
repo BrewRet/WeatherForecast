7d7b15de4ba2d9a5fd6b4b0cb68a30e91bf3c271
BrewRet <dudefist@mail.ru>
2026-02-16 10:54:46 +0700

    Сооздание кофигурационного файла с настройками проекта

---
+++ app/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    app_name: str
+    version: str
+    host: str
+    port: int
+    debug: bool
+    db_path: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", case_sensitive=False, extra="ignore"
+        )
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
