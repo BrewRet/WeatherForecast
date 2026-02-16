68d4ee489393b95a3e3f7c576eba2df7887b9e48
BrewRet <dudefist@mail.ru>
2026-02-16 10:58:07 +0700

    Реализация получения погоды в данный момент по координатам, создание каркаса для остальных методов

---
+++ app/routers.py
@@ -0,0 +1,78 @@
+from fastapi import APIRouter, status, Depends
+
+import aiosqlite
+
+from app.dependancy import get_db
+from app.schemas import (
+    CurrentResponse,
+    CityRequestParams,
+    CitiesResponse,
+    CityCreate,
+    CityResponse,
+)
+from app.services import (
+    get_current_weather_to_show,
+)
+
+router = APIRouter(
+    prefix="/weather",
+    tags=["weather"],
+)
+
+
+@router.get(
+        "/current", 
+        response_model=CurrentResponse,
+        status_code=status.HTTP_200_OK,
+        summary="Получить погоду по координатам",
+)
+async def get_current_weather(lat: float, lon: float) -> CurrentResponse:
+    """
+    Получить погоду в момент запроса.
+    
+    - **lat**: долгота
+    - **lon**: широта
+    """
+    data = await get_current_weather_to_show(lat, lon)
+    return CurrentResponse(
+        temperature=data["temperature"],
+        wind_speed=data["wind_speed"],
+        atmospheric_pressure=data["atmospheric_pressure"],
+    )
+
+
+@router.post(
+        "/cities",
+        
+)
+async def add_city_with_coords(
+    request: CityCreate,
+    db: aiosqlite.Connection = Depends(get_db)):
+
+
+    pass
+
+
+@router.get(
+        "/cities",
+        response_model=CitiesResponse,
+)
+async def get_cities_with_weather_forecast(db: aiosqlite.Connection = Depends(get_db)) -> CitiesResponse:
+
+
+    pass
+
+
+@router.get(
+        "/cities/{city}/{time}",
+        response_model=CityResponse
+)
+async def get_weather_from_city_at_time(
+    city: str,
+    time: str, 
+    params: CityRequestParams,
+    db: aiosqlite.Connection = Depends(get_db),
+    ) -> CityResponse:
+
+
+    pass
